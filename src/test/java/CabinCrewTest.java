61a088330f538c646b957c10811a91e023b72ee6
Craig Smith <craig_ag_smith@outlook.com>
2021-08-28 14:33:31 +0100

    cabin crew

---
+++ src/test/java/CabinCrewTest.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CabinCrewTest {
+
+    CabinCrewMember cabinCrewMember;
+
+    @Before
+    public void before() {
+        cabinCrewMember = new CabinCrewMember("Babz", Rank.CAPTAIN);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Babz", cabinCrewMember.getName());
+    }
+
+    @Test
+    public void hasRank() {
+        assertEquals(Rank.CAPTAIN, cabinCrewMember.getRank());
+    }
+
+
+}
